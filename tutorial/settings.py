a16e0609042f2c2cdcb45bba4368b867c04559d7
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-02-03 14:52:48 -0400

    Added framework configuration missed

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -123,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
