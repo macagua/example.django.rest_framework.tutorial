da3995a4af670acf5037ec4a043ea7232df1903a
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-02-03 14:52:16 -0400

    Added Comment and Event models into admin site

--- quickstart/admin.py
+++ quickstart/admin.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from quickstart.models import Comment, Event
 
 # Register your models here.
+
+admin.site.register(Comment)
+admin.site.register(Event)
