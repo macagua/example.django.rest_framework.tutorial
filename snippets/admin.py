25318717f6f531bf471216cda5832a8d0a4855b3
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-02-07 23:06:12 -0400

    Added Snippets model into Admin site

--- snippets/admin.py
+++ snippets/admin.py
@@ -2,4 +2,12 @@
 
 from django.contrib import admin
 
-# Register your models here.
+from snippets.models import Snippet
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'code', 'linenos', 'language', 'style')
+    list_filter = ['code', 'language']
+    search_fields = ['title', 'code'] # created
+
+admin.site.register(Snippet, SnippetAdmin)
